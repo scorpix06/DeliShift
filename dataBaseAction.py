e9d787f23cc251aabc8d0975c1334df1e1fe157b
scorpix06 <62959825+scorpix06@users.noreply.github.com>
2021-03-23 11:32:35 +0100

    Modify DB

--- dataBaseAction.py
+++ dataBaseAction.py
@@ -3,11 +3,14 @@ import sqlite3
 class dataBaseActions:
 
     def __init__(self):
-        self.dataBase = sqlite3.connect('shift.db')
-        self.cur = dataBase.cursor()
+        self.dataBase = sqlite3.connect('data.db')
+        self.cur = self.dataBase.cursor()
     
     def startShift(self, deliveryMan, startTime, startDate):
         pass
+
+
+dataBaseActions()
 
     
     
