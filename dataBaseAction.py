496dcaade0f97a6fa804cae545999f62b95937a9
scorpix06 <62959825+scorpix06@users.noreply.github.com>
2021-03-23 12:30:43 +0100

    changes

--- dataBaseAction.py
+++ dataBaseAction.py
@@ -1,16 +1,36 @@
 import sqlite3
 
+#  To Do
+#  Add colomn in data.db 
+#  Create founction startShift, stopShift
+
+
 class dataBaseActions:
 
     def __init__(self):
-        self.dataBase = sqlite3.connect('data.db')
+        try:
+            self.dataBase = sqlite3.connect('data.db')
+            print("Succefuly connected to the database")
+        except:
+            print("Failed to connect to the database")
         self.cur = self.dataBase.cursor()
     
     def startShift(self, deliveryMan, startTime, startDate):
         pass
 
+    def addDeliveryMan(self, name):
+        command = """INSERT INTO livreurs (nomLivreur)  VALUES ("{}");""".format(name)
+        self.cur.execute(command)
+        self.dataBase.commit()
 
-dataBaseActions()
+    def delDeliveryMan(self, name):
+        command = """DELETE FROM livreurs WHERE nomLivreur="{}" """.format(name)
+        self.cur.execute(command)
+        self.dataBase.commit()
+
+a = dataBaseActions()
+a.delDeliveryMan("Alban")
+
 
     
-    
+     
